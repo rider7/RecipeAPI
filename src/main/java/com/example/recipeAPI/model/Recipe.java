03f244bdd33b96de84ceac4cca47aae5ea9ea169
rider7 <brandon.rider@gmail.com>
2021-07-17 23:41:09 -0500

    model commit

---
+++ src/main/java/com/example/recipeAPI/model/Recipe.java
@@ -0,0 +1,61 @@
+package com.example.recipeAPI.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity //Used to mark the class as a persistent Java class.
+@Table(name = "recipe") //Used to provide the details of the table that this entity will be mapped to
+@EntityListeners(AuditingEntityListener.class) // Specifies the callback listener classes to be used for an entity or mapped superclass.
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, //Used for Serializing and Deserializing Java objects to and from JSON
+        allowGetters = true)
+public class Recipe implements Serializable { //Implements used so Note class can access Serializable interface methods. Note: Class can implement multiple interfaces at a time.
+    @Id //Used to define the primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Used to define the primary key generation strategy. In the above case, we have declared the primary key to be an Auto Increment field
+    private Long recipe_id;
+
+    @NotBlank //Used to validate that the annotated field is not null or empty
+    private String recipe_name;
+
+    @NotBlank //Used to validate that the annotated field is not null or empty
+    private String recipe_desc;
+
+    @Column(nullable = false, updatable = false) //Used to define the properties of the column that will be mapped to the annotated field
+    @Temporal(TemporalType.TIMESTAMP) //Used with java.util.Date and java.util.Calendar classes. It converts the date and time values from Java Object to compatible database type and vice versa.
+    @CreatedDate //Used to track created date
+    private Date createdAt;
+
+    @Column(nullable = false) //Used to define the properties of the column that will be mapped to the annotated field
+    @Temporal(TemporalType.TIMESTAMP) //Used with java.util.Date and java.util.Calendar classes. It converts the date and time values from Java Object to compatible database type and vice versa.
+    @LastModifiedDate //Used to track modified date
+    private Date updatedAt;
+
+    public Long getRecipe_id() {
+        return recipe_id;
+    }
+
+    public void setRecipe_id(Long recipe_id) {
+        this.recipe_id = recipe_id;
+    }
+
+    public String getRecipe_name() {
+        return recipe_name;
+    }
+
+    public void setRecipe_name(String recipe_name) {
+        this.recipe_name = recipe_name;
+    }
+
+    public String getRecipe_desc() {
+        return recipe_desc;
+    }
+
+    public void setRecipe_desc(String recipe_desc) {
+        this.recipe_desc = recipe_desc;
+    }
+}
---
+++ src/main/java/com/example/recipeAPI/repository/RecipeRepository.java
@@ -0,0 +1,10 @@
+package com.example.recipeAPI.repository;
+
+import com.example.recipeAPI.model.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+
+}
