5be64d11bd6a25e7824ee93638bd59cf79fd9f01
rider7 <brandon.rider@gmail.com>
2021-07-17 23:20:20 -0500

    Update RecipeApiApplication.java
    imported jpa for auditing

--- src/main/java/com/example/recipeAPI/RecipeApiApplication.java
+++ src/main/java/com/example/recipeAPI/RecipeApiApplication.java
@@ -2,10 +2,15 @@ package com.example.recipeAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+//Entry point of application
+//Annotation used for to enable auto-configuration, component scan and allow import of additional configuration classes
 @SpringBootApplication
-public class RecipeApiApplication {
+@EnableJpaAuditing //Enables auditing via annotation configuration (needed for use of @CreatedDate and @LastModifiedDate annotations)
 
+public class RecipeApiApplication {
+	//Call to launch the spring application
 	public static void main(String[] args) {
 		SpringApplication.run(RecipeApiApplication.class, args);
 	}
